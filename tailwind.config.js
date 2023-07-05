1b9b709ddda2ed919c2322337f40bbc1365050ec
norbertbla <blaszczyk.norbert@outlook.com>
2023-07-05 19:02:59 +0000

    Added purge for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
 export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./node_modules/daisyui/**/*.{js,ts,jsx,tsx}",
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+  './app/**/*.{js,ts,jsx,tsx}',
+  '!./node_modules/daisyui/**/*.{js,ts,jsx,tsx}',
 ];
-
+export const purge = ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'];
 export const theme = {
   extend: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px", // (min-width: 1536px)
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px', // (min-width: 1536px)
     },
     transitionProperty: {
-      width: "width",
+      width: 'width',
     },
   },
 };
 
-export const darkMode = ["class", '[data-theme="dark"]'];
+export const darkMode = ['class', '[data-theme="dark"]'];
 
 export const daisyui = {
-  themes: ["dark"]
+  themes: ['dark'],
 };
 
-export const plugins = [require("daisyui")];
+export const plugins = [require('daisyui')];
