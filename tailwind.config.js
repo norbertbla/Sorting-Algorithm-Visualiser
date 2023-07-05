794df69b4883713a2394f74614da990ca199a98f
norbertbla <blaszczyk.norbert@outlook.com>
2023-07-05 18:43:08 +0000

    Added daisyui content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: ["class", '[data-theme="dark"]'],
-  daisyui:{
-    themes: ["dark"]
-  },
-  theme: {
-    extend: {
-      screens: {
-        sm: "640px", // (min-width: 640px)
-        md: "768px", // (min-width: 768px)
-        lg: "1024px", // (min-width: 1024px)
-        xl: "1280px", // (min-width: 1280px)
-        "2xl": "1536px", // (min-width: 1536px)
-      },
-      transitionProperty: {
-        width: "width",
-      },
+
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/daisyui/**/*.{js,ts,jsx,tsx}",
+];
+
+export const theme = {
+  extend: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px", // (min-width: 1536px)
+    },
+    transitionProperty: {
+      width: "width",
     },
   },
-  plugins: [require("daisyui")],
 };
+
+export const darkMode = ["class", '[data-theme="dark"]'];
+
+export const daisyui = {
+  themes: ["dark"]
+};
+
+export const plugins = [require("daisyui")];
